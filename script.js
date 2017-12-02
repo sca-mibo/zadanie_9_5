5959e66457663e0fab676032cc04e52a80110a6b
Michal Bobik <mikebobik1811@gmail.com>
2017-12-02 18:50:03 +0100

    Zadanie_9_5_poprawione

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 	var elemWithButtonClass = document.getElementsByClassName('button');
 	console.log(elemWithButtonClass);
-	for (var i = 0; i <= elemWithButtonClass.length; i++){
+	for (var i = 0; i < elemWithButtonClass.length; i++){
 		console.log(elemWithButtonClass[i].innerText);
 		alert(elemWithButtonClass[i].innerText);
 	}
